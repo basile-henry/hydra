29e9351866c439f6160b96ef554d645eb6b24474
Basile Henry <bjm.henry@gmail.com>
2023-10-04 09:35:15 +0100

    first broken commit

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 # The `default.nix` in flake-compat reads `flake.nix` and `flake.lock` from `src` and
 # returns an attribute set of the shape `{ defaultNix, shellNix }`
 
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/35bb57c0c8d8b62bbfd284272c928ceb64ddbde9.tar.gz) {
+(import (fetchTarball https://github.com/edolstra/flake-compat/archive/bc5e257a8d0c4df04652ecff9053d05b0dc9484e.tar.gz) {
   src = ./.;
 }).defaultNix
